d0e57cd38eb2a7766d82b009f0d6c03d553cae38
Gurpreet  Singh <gopi89410gmail.com>
2023-08-06 18:46:53 -0400

    created and add nmp

---
+++ script.js
@@ -0,0 +1,9 @@
+const scroller = document.querySelector("body");
+const nav = document.querySelector(".Mynavbar");
+
+scroller.addEventListener("scroll", (event) => {
+    if(scroller.scrollTop>2){
+        
+
+    }
+});
